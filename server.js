e138afb0e1dee51b232ae7d0f45056e00918c682
Lucas Silva <lucasmaiaesilva@gmail.com>
2020-05-16 22:59:04 -0300

    add socketio toggle logic

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
-// 'use strict';
-//
-// const express = require('express');
-// const socketIO = require('socket.io');
-//
-// const PORT = process.env.PORT || 3000;
-// const INDEX = '/index.html';
-//
-// const server = express()
-//   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-//
-// const io = socketIO(server);
-//
-// io.on('connection', (socket) => {
-//   console.log('Client connected');
-//   socket.on('disconnect', () => console.log('Client disconnected'));
-// });
-//
-// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT);
+let officeState = false
+
+server.listen(PORT, () => console.log(`connectado em porta ${PORT}`));
 // WARNING: app.listen(80) will NOT work here!
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/escritorio', (req, res) => {
+  res.json({ office: officeState })
+});
+
+app.get('/escritorio/:newState', (req, res) => {
+  if (req.params.newState === 'on') {
+    officeState = true;
+    io.emit('update', { office: officeState });
+    return res.json({ error: false, message: 'ok' });
+  }
+  if (req.params.newState === 'off') {
+    officeState = false;
+    io.emit('update', { office: officeState });
+    return res.json({ error: false, message: 'ok' });
+  }
+  return res.json({ error: true, message: 'command invalid' });
+});
+
 io.on('connection', (socket) => {
-  console.log('Conectado porra')
+  console.log('Client connected')
   // socket.emit('news', { hello: 'world' });
-  socket.on('my other event', (data) => {
-    console.log(data);
-  });
+  // socket.on('my other event', (data) => {
+  //   console.log(data);
+  // });
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
