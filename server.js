b2ccbf5b3ed3f346f198ecd63a6c41c787401d53
Lucas Silva <lucasmaiaesilva@gmail.com>
2020-05-16 22:11:09 -0300

    add heroku test

--- server.js
+++ server.js
@@ -1,20 +1,40 @@
-'use strict';
+// 'use strict';
+//
+// const express = require('express');
+// const socketIO = require('socket.io');
+//
+// const PORT = process.env.PORT || 3000;
+// const INDEX = '/index.html';
+//
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+//
+// const io = socketIO(server);
+//
+// io.on('connection', (socket) => {
+//   console.log('Client connected');
+//   socket.on('disconnect', () => console.log('Client disconnected'));
+// });
+//
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
-const express = require('express');
-const socketIO = require('socket.io');
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+server.listen(80);
+// WARNING: app.listen(80) will NOT work here!
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const io = socketIO(server);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+  // socket.emit('news', { hello: 'world' });
+  socket.on('my other event', (data) => {
+    console.log(data);
+  });
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
